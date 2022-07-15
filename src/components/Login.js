1ececdafeb4c98b574b58fc7ab339de95ff19c16
Steven-Padilla <steven_padilla@.github.com>
2022-07-15 06:34:39 -0500

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+
+
+export default function Login(){
+    return(
+        <div className="form">
+            <form>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {/* {renderErrorMessage("uname")} */}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {/*renderErrorMessage("pass")*/}
+                </div>
+                <div className="button-container">
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
+    )
+}

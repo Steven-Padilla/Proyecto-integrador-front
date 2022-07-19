e76c91eda4bb05aad360c5ea4ef19d080edf5d9d
Steven-Padilla <steven_padilla728@outlook.com>
2022-07-18 21:53:22 -0500

    Background resolved and routes were protrected

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import style from './css/Footer.module.css'
-
-export const Footer =()=>{
-    return (
-        <div className={style.container}>
-            Todos los derechos reservados
-        </div>
-    )
-}
